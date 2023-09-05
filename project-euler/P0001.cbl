1846da1b75421dd1e6302f9cf558a3343078f5b7
eddtests <edward@haigh.se>
2023-09-05 09:48:36 +0200

    Initial code

---
+++ project-euler/P0001.cbl
@@ -0,0 +1,11 @@
+>> SOURCE FORMAT FREE
+id division.
+program-id. P0001.
+author. eddtests.
+date-written. September 5th 2023.
+environment division.
+data division.
+working-storage section.
+01 Answer PIC 999999999 VALUE 0.
+procedure division.
+display "Hello, world!" end-display.
